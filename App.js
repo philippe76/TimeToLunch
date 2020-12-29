b93a4b24db1d39b1cb8aaa7b3c8365237fa2dd23
Philippe Lanougadere <soufiane92230@gmail.com>
2020-12-29 15:53:31 +0100

    First step alone in HomePage

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>  
+      <Text style={styles.textTitle}>Time's To Lunch</Text>
     </View>
   );
 }
@@ -14,8 +12,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textTitle: {
+    color: '#e7af77',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
