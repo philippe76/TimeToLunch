8c3338e03eb2226c49a11ef0591637c96f50061d
Philippe Lanougadere <soufiane92230@gmail.com>
2020-12-29 19:39:39 +0100

    Update HomePage and Create  menu tabs

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import Home from './components/Home';
 import AppLoading from 'expo-app-loading';
 
+
 const getFonts = () => Font.loadAsync({
-  'Pacifico-Regular': require('./assets/font/Pacifico-Regular.ttf')
+  'Pacifico-Regular': require('./assets/font/Pacifico-Regular.ttf'),
+  'CrimsonText-SemiBold': require('./assets/font/CrimsonText-SemiBold.ttf')
 })
+
+
 
 export default function App() {
 
   const [fontsLoaded, setFontsLoaded] = useState(false)
+
 
   if (fontsLoaded) {
     return (
--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const Home = () => {
     return (
+        <>
       <View style={styles.container}>  
+        <View style={styles.logoContainer}>
+            <Text style={styles.logoTitle}>Sweet Home</Text>    
+        </View>  
         <Text style={styles.textTitle}>Time's To Lunch</Text>
       </View>
+      <View style={styles.menu}>
+        <TouchableOpacity style={styles.lunchContainer}>
+            <Text style={{...styles.menuText, ...styles.lunchText}}>Lunch</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.breakfastContainer}>
+            <Text style={{...styles.menuText, ...styles.breakfastText}}>Breakfast</Text>
+        </TouchableOpacity>
+      </View>
+      </>
     );
 }
 
@@ -16,12 +29,57 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logoContainer: {
+    width: 230,
+    height: 230,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 230/2,
+    borderWidth: 2,
+    borderColor: '#e7af77',
+    position: 'absolute',
+    top: 100
+  },
+  logoTitle: {
+    color: '#e7af77',
+    fontSize: 35,
+    fontFamily: 'Pacifico-Regular'
+  },
   textTitle: {
     color: '#e7af77',
-    fontSize: 20,
-    fontWeight: 'bold',
-    fontFamily: 'Pacifico-Regular'
+    fontSize: 45,
+    fontFamily: 'Pacifico-Regular',
+    marginTop: 50
+  },
+  menu: {
+    backgroundColor: '#e7af77',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  lunchContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height:60
+  },
+  breakfastContainer: {
+    backgroundColor: '#2b3d28',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    height:60
+  },
+  menuText: {
+    fontFamily: 'CrimsonText-SemiBold',
+    fontSize: 25
+  },
+  lunchText: {
+    color: '#2b3d28'
+  },
+  breakfastText: {
+    color: '#e7af77'
   }
+
 });
 
 export default Home;
