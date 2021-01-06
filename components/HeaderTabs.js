6bf914914b5cb23447c47dae364bc2db4485528e
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-06 22:29:30 +0100

    Customize HeaderTab

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Entypo, FontAwesome5 } from '@expo/vector-icons';
+import { pumpkin, forest } from '../styles/colors';
 
 import Home from '../screens/Home';
 import Order from '../screens/Order';
@@ -9,25 +10,34 @@ import Order from '../screens/Order';
 const Tab = createMaterialTopTabNavigator();
 
 const HeaderTabs = () => {
+
+    const tabBarOptions = {
+        indicatorStyle: { 
+            backgroundColor: pumpkin,
+
+        },
+    }
+
     return (
         <Tab.Navigator
             initialRouteName="Home"
             style={styles.header}
-            screenOptions={() => ({
-                tabBarIcon: () => {
-                        return  <Entypo name="home" style={styles.homeIcon} size={30} />
-                    }
-            })}
+            tabBarOptions={tabBarOptions}
+
         >
             <Tab.Screen 
                 name="Home"
-                component={Home}                    
+                component={Home}  
+                options={{  
+                    tabBarLabel: () => <Entypo name="home" style={styles.homeIcon} size={30} />                   
+                }}
+                              
             />
             <Tab.Screen 
                 name="Order"
                 component={Order}
                 options={{
-                    tabBarIcon: 'bell-outline'
+                    tabBarLabel: () => <FontAwesome5 name="shopping-bag" style={styles.bagIcon} size={28} />
                 }}                          
             />
         </Tab.Navigator>
@@ -36,7 +46,16 @@ const HeaderTabs = () => {
 
 const styles = StyleSheet.create({
     header: {
-        marginTop: 50
+        marginTop: 50,
+        backgroundColor: '#f0f0f0',
+    },
+    homeIcon: {
+        color: pumpkin,
+        padding: 10,
+    },
+    bagIcon: {
+        color: pumpkin,
+        padding: 10
     }
 })
 
